0e476029b537a6dc4b7ade425b62ff6b336949cd
mwangiii <mwangi8kevin@gmail.com>
2023-08-05 10:56:02 +0300

    fixing webflask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""Starts flask app and routes two requests
+"""
+Script that starts a Flask web application:
 
-    Returns:
-        string: normal strings
-    """
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display "HBNB"
+"""
 from flask import Flask
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def home():
+@app.route("/")
+def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route("/hbnb")
 def hbnb():
     return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Starts a flask app and renders list of states
+"""
+Script that starts a Flask web application:
 
-    Returns:
-        file: Contains list of states
-    """
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+       - H1 tag: “States”
+       - UL tag: with the list of all State objects sorted by name
+       - LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
-    all_states = storage.all(State)
-    sorted_states = sorted(all_states.values(), key=lambda state: state.name)
+    states_list = storage.all(State)
+    sorted_list = sorted(states_list.values(), key=lambda state: state.name)
 
-    return render_template("7-states_list.html", states=sorted_states)
+    return render_template("7-states_list.html", states=sorted_list)
 
 
 @app.teardown_appcontext
@@ -26,4 +30,4 @@ def teardown(td):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
